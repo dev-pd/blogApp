4725b2d280e9303de2256ccc634c7f032e674236
deshpandeprasad <deshpande.prasa@northeastern.edu>
2024-08-02 20:46:11 -0400

    created the post repository

--- src/main/java/com/blog/blogApp/repository/PostRepository.java
+++ src/main/java/com/blog/blogApp/repository/PostRepository.java
@@ -1,2 +1,7 @@
-package com.blog.blogApp.repository;public interface PostRepository {
+package com.blog.blogApp.repository;
+
+import com.blog.blogApp.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
