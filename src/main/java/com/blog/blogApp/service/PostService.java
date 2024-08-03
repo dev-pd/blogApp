69ae5ab46756d7d11a4e26979a7e95bcdf2b6983
deshpandeprasad <deshpande.prasa@northeastern.edu>
2024-08-02 22:08:16 -0400

    pushed dto, service, serviceimpl

---
+++ src/main/java/com/blog/blogApp/service/PostService.java
@@ -0,0 +1,7 @@
+package com.blog.blogApp.service;
+
+import com.blog.blogApp.payload.PostDTO;
+
+public interface PostService {
+    PostDTO createPost(PostDTO postDTO);
+}
---
+++ src/main/java/com/blog/blogApp/service/impl/PostServiceImpl.java
@@ -0,0 +1,42 @@
+package com.blog.blogApp.service.impl;
+
+import com.blog.blogApp.entity.Post;
+import com.blog.blogApp.payload.PostDTO;
+import com.blog.blogApp.repository.PostRepository;
+import com.blog.blogApp.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    private PostRepository postRepository;
+
+    @Autowired
+    public PostServiceImpl(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public PostDTO createPost(PostDTO postDTO) {
+
+        //The DTO is received and then it is converted to Entity
+        Post post = new Post();
+        post.setTitle(postDTO.getTitle());
+        post.setDescription(postDTO.getDescription());
+        post.setContent(postDTO.getContent());
+
+        //Entity is then saved to the DB
+        //Returns an Entity which is saved
+        Post savedPost = postRepository.save(post);
+
+        //The saved Entity is then converted to DTO to transfer
+        //Decouple DTO and Entity
+        PostDTO postDTOResponse = new PostDTO();
+        postDTOResponse.setTitle(savedPost.getTitle());
+        postDTOResponse.setDescription(savedPost.getDescription());
+        postDTOResponse.setContent(savedPost.getContent());
+
+        return postDTOResponse;
+    }
+}
